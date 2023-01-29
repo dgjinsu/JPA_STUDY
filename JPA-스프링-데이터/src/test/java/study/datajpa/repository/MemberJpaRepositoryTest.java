b05082458dbff73841eb1bce0f5e5a7d1d8cd174
dgjinsu <rlawlstn3206@naver.com>
2023-01-30 01:53:22 +0900

    jpa_study

--- JPA-스프링-데이터/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ JPA-스프링-데이터/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -8,6 +8,9 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -15,6 +18,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @Rollback(value = false)   // 스프링에서 테스트 코드는 db에 아무 쿼리도 보내지 않음. 쿼리를 보고 싶으면 rollback 을 false 로 하면 보임
 class MemberJpaRepositoryTest {
     @Autowired MemberJpaRepository memberJpaRepository;
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Test
     public void testMember() {
@@ -25,10 +30,40 @@ class MemberJpaRepositoryTest {
         //when
         Member findMember = memberJpaRepository.find(savedMember.getId());
         //then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(member);
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
 
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        //단건 조회 검증
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        //리스트 조회 검증
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        //카운트 검증
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long after_delete_count = memberJpaRepository.count();
+        assertThat(after_delete_count).isEqualTo(0);
     }
 
 }
